86edf3be7fc9995766ec55b201efa1b8e37eeace
Henrik Bengtsson <henrikb@braju.com>
2011-08-01 00:00:00 +0000

    version 0.4.3

---
+++ R/999.package.R
@@ -0,0 +1,37 @@
+#########################################################################/**
+# @RdocPackage ACNE
+#
+# \description{
+#   @eval "getDescription(ACNE)" 
+#
+#   This package should be considered to be in an alpha phase.
+#   You should expect the API to be changing over time.
+# }
+#
+# \section{Requirements}{
+#   This package requires the \pkg{aroma.affymetrix} package.
+# }
+#
+# \section{Installation and updates}{
+#   To install this package, see instructions at
+#   \url{http://r-forge.r-project.org/projects/snpsprocessing/}.
+# } 
+#
+# \section{To get started}{
+#   See the @see "NmfPlm" class.
+# }
+# 
+# \author{
+#   Angel Rubio <arubio@ceit.es>,
+#   Maria Ortiz <mortiz@ceit.es>, 
+#   Henrik Bengtsson <henrikb@braju.com>
+# }
+#
+# \section{License}{
+#   @eval "getLicense(ACNE)"
+# }
+# 
+# \references{
+#   TBA.
+# }
+#*/#########################################################################
---
+++ R/zzz.R
@@ -0,0 +1,18 @@
+# Allows conflicts. For more information, see library() and
+# conflicts() in [R] base.
+.conflicts.OK <- TRUE;
+
+# WORKAROUND: In order for the package to work with the most recent
+# version of R devel, which automatically add namespaces to packages
+# who do not have one, we explicitly have specify the following.
+# /HB 2011-08-01
+# From R.utils:
+cat <- R.utils::cat; 
+
+.First.lib <- function(libname, pkgname) {
+  pkg <- Package(pkgname);
+  assign(pkgname, pkg, pos=getPosition(pkg));
+
+  packageStartupMessage(getName(pkg), " v", getVersion(pkg), " (", 
+    getDate(pkg), ") successfully loaded. See ?", pkgname, " for help.");
+}
